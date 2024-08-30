d08106df7563dd401a97591db8f7f706e3c78e0c
EvanSamim <seikhevan08@gmail.com>
2024-08-30 15:56:56 +0530

    This is my first Commit

---
+++ src/entity/Player.java
@@ -0,0 +1,10 @@
+package entity;
+
+import main.GamePanel;
+import main.KeyHandler;
+
+public class Player extends Entity{
+    public Player(GamePanel gp, KeyHandler keyH) {
+        super(gp, keyH);
+    }
+}
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -15,8 +15,8 @@ public class GamePanel extends JPanel implements Runnable{
 
     public final int tileSize = originalTileSize * scale;
 
-    public final int screenCol = 20;
-    public final int screenRow = 11;
+    public final int screenCol = 21;
+    public final int screenRow = 13;
 
     public final int screenWidth = tileSize * screenCol;
     public final int screenHeight = tileSize * screenRow;
@@ -26,6 +26,7 @@ public class GamePanel extends JPanel implements Runnable{
 
 
     // SYSTEM
+    KeyHandler keyH = new KeyHandler(this);
     Thread gameThread;
 
 
@@ -36,6 +37,8 @@ public class GamePanel extends JPanel implements Runnable{
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -47,5 +50,43 @@ public class GamePanel extends JPanel implements Runnable{
     @Override
     public void run() {
 
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        double lastTime = System.nanoTime();
+        double currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null) {
+
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if (timer >= 1000000000) {
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.dispose();
     }
 }
