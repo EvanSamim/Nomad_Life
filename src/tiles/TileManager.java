2d0d2863870f935d6eef49e0040e823fce35c053
EvanSamim <seikhevan08@gmail.com>
2024-09-23 09:04:53 +0530

    This is my first Commit

--- src/tiles/TileManager.java
+++ src/tiles/TileManager.java
@@ -5,13 +5,14 @@ import main.UtilityTool;
 
 import javax.imageio.ImageIO;
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class TileManager {
 
     public Tile[] tile;
     public int[][] mapTileNum;
+
+    private String filePath;
 
     GamePanel gp;
     public TileManager(GamePanel gp) {
@@ -24,26 +25,55 @@ public class TileManager {
 
     public void getTileImage() {
 
-        setup(0, "steps_01", false);
+        setup("steps", 0, "steps_01", false);
+        setup("grass_tiles", 1, "grass_path_07", false);
     }
 
-    public void setup(int index, String fileName, boolean collision) {
+    public void setup(String filePath, int index, String fileName, boolean collision) {
         UtilityTool uTool = new UtilityTool();
 
         try {
 
             tile[index] = new Tile();
-            tile[index].image = ImageIO.read(getClass().getResourceAsStream("/tile/" + fileName + ".png"));
-            tile[index].image = uTool.scaleImage(tile[index].image, gp.tileSize, gp.tileSize*2);
+            tile[index].image = ImageIO.read(getClass().getResourceAsStream("/" + filePath +"/" + fileName + ".png"));
+            switch (filePath) {
+                case "steps": tile[index].image = uTool.scaleImage(tile[index].image, gp.tileSize*2, gp.tileSize*4); break;
+                case "grass_tiles": tile[index].image = uTool.scaleImage(tile[index].image, gp.tileSize, gp.tileSize); break;
+            }
             tile[index].collision = collision;
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+        this.filePath = filePath;
     }
 
     public void draw(Graphics2D g2) {
 
-        g2.drawImage(tile[0].image, 0, 0, gp.tileSize*2, gp.tileSize*4, null);
+        int worldCol = 0;
+        int worldRow = 0;
+
+        while (worldCol <= gp.screenWidth && worldRow <= gp.screenHeight) {
+
+            int num = 0;
+            switch (num) {
+                case 0:
+                    g2.drawImage(tile[0].image, worldCol, worldRow, null);
+                    worldCol += gp.tileSize*2;
+                    break;
+                case 1:
+                    g2.drawImage(tile[1].image, worldCol, worldRow, null);
+                    worldCol += gp.tileSize;
+                    break;
+            }
+
+            if (worldCol == gp.screenCol) {
+                worldCol = 0;
+                switch (num) {
+                    case 0: worldRow += gp.tileSize*2; break;
+                    case 1: worldRow += gp.tileSize; break;
+                }
+            }
+        }
     }
 }
